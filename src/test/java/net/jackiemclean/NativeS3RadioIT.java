fe423a8252aac9c84855f96f84961f8701648a14
Mikey Austin <mikey.austin@odem.io>
2020-10-19 15:15:36 +0200

    remove broken tests

--- src/test/java/net/jackiemclean/NativeS3RadioIT.java
+++
@@ -1,9 +0,0 @@
-package net.jackiemclean;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeS3RadioIT extends S3RadioTest {
-
-    // Execute the same tests but in native mode.
-}
